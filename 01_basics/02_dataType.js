a5b8e3aea8e48851778ee630f1e7be6342e152a7
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2024-09-25 13:08:13 +0530

    conatiner config done

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (5+5) // we are using node js , not browser
+
+console.log(3+3);
+console.log("junaid");
+
+let name ="junaid";
+let age = 18;
+let isLoggedIn = false;
+
+console.table([name,age,isLoggedIn]);
+
+console.log(typeof name);
