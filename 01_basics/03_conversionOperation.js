e5e873df89245447702760d9ae2380f1113e9863
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2024-09-25 15:56:12 +0530

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = false
 
-console.log(typeof (score))
-console.log(typeof score)
+//console.log(typeof (score))
+//console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 
 //"33" => 33
 //"33abc" => NaN
@@ -13,7 +13,7 @@ console.log(valueInNumber)
 
 let isLoggedIn = "junaid"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1=> true; 0 => false
 // "" => false
@@ -21,4 +21,37 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+
+//************************************** Operations *************************************
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+let srt1 ="hello"
+let str2 = " junaid"
+let str3 = srt1 + str2
+//console.log(str3)
+
+//console.log("1" + 2)
+//console.log(1 + "2")
+//console.log("1" + 2 +2 )
+//console.log(1 + 2 + "2")
+
+//console.log(true)
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+//console.log(gameCounter)
+
+let x= 3
+const y = x++
+
+console.log(`x:${x}, y:${y}`)
+
+let a= 3
+const b = ++x
+console.log(`a:${a}, b:${b}`)
