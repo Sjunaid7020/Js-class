7eee45676415a98a37e93a7881e9b34eda8a0c9f
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2024-10-01 22:31:15 +0530

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1); its ! =
+
+//console.log('2' > 1);
+//console.log('02' > 1);
+
+
+console.log(null> 0);
+console.log(null == 0); //its = =
+console.log(null >= 0);//its > =
+
+console.log( 0 === 0 );
+
+console.log("2" + 1);
+console.log("2" - 1);
