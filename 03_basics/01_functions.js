9c74e4d28311ea219799370006d2116382671256
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2024-12-21 16:44:41 +0530

    functios starts

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,41 @@
+//console.log("j")
+//console.log("u")
+//console.log("n")
+//console.log("a")
+//console.log("i")
+//console.log("d")
+
+function  sayMyName(){
+    console.log("j")
+    console.log("u")
+    console.log("n")
+    console.log("a")
+    console.log("i")
+    console.log("d")
+}
+
+//sayMyName()
+
+//function addTwoNum(num1,num2){
+//    console.log(num1 + num2)
+//}
+
+function addTwoNum(num1,num2){
+//    let result = num1 + num2
+//    return result
+
+    return num1 + num2
+}
+const result= addTwoNum(3,5)
+//console.log("result:",result)
+
+function loginUserMessage(username = "Xyz"){
+    if (!username){
+        console.log("please enter a username")
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Junaid"))
+//console.log(loginUserMessage())
