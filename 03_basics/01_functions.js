9afd3c5e11c3b20e07dc978bd68b2ab8665e6f07
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2024-12-26 16:52:02 +0530

    functions done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -43,4 +43,26 @@ function loginUserMessage(username = "Xyz"){
 function calculateCartPrice(...num1){
     return num1
 }
-console.log(calculateCartPrice(200,400,500))
+//console.log(calculateCartPrice(200,400,500))
+
+const user ={
+    username: "name",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+//handleObject(user)
+handleObject({
+    username:'sam',
+    price:233
+})
+
+const myNewArray =[200,400,600,800]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
