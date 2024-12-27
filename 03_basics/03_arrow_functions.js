711ae08cd7c75e05e926d1369e8b38685ccf4c5a
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2024-12-27 18:25:33 +0530

    arrow function done

---
+++ 03_basics/03_arrow_functions.js
@@ -0,0 +1,42 @@
+const user ={
+    username: 'Xyz',
+    price:199,
+
+    welcomeMessage:function (){
+        console.log(`${this.username}, wellcome to website`)
+        console.log(this)
+    }
+}
+
+//user.welcomeMessage()
+//user.username='Sam'
+//user.welcomeMessage()
+
+//console.log(this)
+
+//function one() {
+//    let username= 'one'
+//    console.log(this.username)
+//}
+//
+//one()
+
+const one = () => {
+    let  username='one'
+    console.log(this.username)
+}
+
+//one()
+
+//const  addtwo = (num1,num2) => {
+//    return num1 + num2
+//}
+
+//const  addtwo = (num1,num2) => num1 + num2
+
+//const  addtwo = (num1,num2) => (num1 + num2)
+
+
+const  addtwo = (num1,num2) => ({username:"ABC"})
+
+console.log(addtwo(5,8))
