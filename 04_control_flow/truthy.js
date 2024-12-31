1c1038f14188327a832f6b4ebfe51b6b36a1bcb6
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2024-12-31 20:23:00 +0530

    Nullish Coalescing Operator Done

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -16,7 +16,23 @@ if (userEmail){
 // console.log('Array is empty')
 //}
 
-const emptyObj = {}
-if (Object.keys(emptyObj).length===0){
-    console.log('Object is empty')
-}
+//const emptyObj = {}
+//if (Object.keys(emptyObj).length===0){
+//    console.log('Object is empty')
+//}
+
+//Nullish Coalescing Operatoe (??): null undefined
+
+let val1;
+//val1= 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1)
+
+//Terniary Operator
+
+//condition ? true : false
+const  icePrice =100
+icePrice <=80 ? console.log('less then 80') :  console.log('more then 80') 
