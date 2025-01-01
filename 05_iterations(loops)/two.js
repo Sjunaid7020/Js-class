f8aa2be6c349d0d698b00367b007c3246d338bd9
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2025-01-01 16:54:18 +0530

    while and do while lopp done

---
+++ 05_iterations(loops)/two.js
@@ -0,0 +1,24 @@
+//while loop
+
+
+let i = 1
+//while (i<=10){
+//    console.log(`value of i is ${i}`)
+//    i = i +2
+//}
+
+let myArray =['flash','batman','superman']
+let arr =0
+while (arr < myArray.length){
+    console.log(`value of i is ${myArray[arr]}`)
+    arr = arr +1
+}
+
+
+//do while loop
+
+let score = 11
+do {
+    console.log(`score is ${score}`)
+    score++
+}while (score <=10);
