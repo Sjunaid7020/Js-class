043856cb1c75c5e74d35d74151d51bd3e5cf6c6a
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2025-01-03 20:59:15 +0530

    reduce done

--- 05_iterations(loops)/eight.js
+++ 05_iterations(loops)/eight.js
@@ -5,8 +5,32 @@ const myNums=[1,2,3]
 //}, 0)
 
 myTotal = myNums.reduce((acc,currval)=>{
-    console.log(`acc:- ${acc} and currval:- ${currval}`)
+    //console.log(`acc:- ${acc} and currval:- ${currval}`)
     return acc + currval
 }, 0)
 
-console.log(myTotal)
+const shoppingCart = [
+    {
+        itemName:'Js course',
+        price:999
+    },
+    {
+        itemName:'CPP course',
+        price:699
+    },
+    {
+        itemName:'Py course',
+        price:999
+    },
+    {
+        itemName:'mobile Dev',
+        price:2999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc,item) =>{
+    console.log(`acc:- ${acc} and currval:- ${item.price}`)
+   return acc + item.price
+},0)
+
+console.log(priceToPay)
