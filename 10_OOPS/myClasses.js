a78bba0768b44a8dc1630367a5d8bb89c16557b9
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2025-02-04 00:35:17 +0530

    classes in js explain

---
+++ 10_OOPS/myClasses.js
@@ -0,0 +1,40 @@
+//es6
+
+// class User {
+//     constructor(username, email,password){
+//         this.username = username
+//         this.email = email
+//         this.password = password
+//     }
+//     encryptPassword(){
+//         return `${this.password}abc`   
+//     }
+//     createUppercase(){
+//         return`${this.username.toUpperCase()}`
+//     }
+// }
+
+// const chai = new User('chai','chai@example.com','123');
+
+// console.log(chai.encryptPassword());
+// console.log(chai.createUppercase());
+
+//behind the scene
+
+function User(username, email,password) {
+    this.username = username
+        this.email = email
+        this.password = password
+}
+
+User.prototype.encryptPassword = function() {
+    return `${this.password}abc`
+}
+User.prototype.createUppercase=function() {
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new User('tea','tea@example.com','456');
+
+console.log(tea.encryptPassword());
+console.log(tea.createUppercase());
