21aa461a02a8a2687f95d91360a2f3cf0f8aebd5
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2025-02-07 19:41:15 +0530

    explain

--- 10_OOPS/inheritance.js
+++ 10_OOPS/inheritance.js
@@ -22,3 +22,8 @@ class Teacher extends User{
 }
 const chai = new Teacher('chai','chai@example.com','123')
 chai.addCourse()
+
+const masalaChai = new User('masalaChai') 
+masalaChai.logMe()
+
+console.log(chai instanceof User);
